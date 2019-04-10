1f71c975813fe41da38a55c2edc087abe75100cf
Ashik S S Gowda <ashikssgowda644@gmail.com>
2019-04-10 15:29:06 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,57 @@
+import { SectionComponent } from './section/section.component';
+import { ToolsComponent } from './tools/tools.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule,MatCheckboxModule,MatMenuModule,MatSelectModule,MatIconModule,MatToolbarModule} from '@angular/material';
+import '@angular/material/prebuilt-themes/deeppurple-amber.css';
+
+
+import { CommonModule } from '@angular/common';
+
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { ModalModule } from 'ngx-bootstrap/modal';
+import {MenuComponent} from './menu/menu.component';
+import { EditorComponent } from './editor/editor.component';
+import { ViewComponent } from './view/view.component';
+
+
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    MenuComponent,
+    ToolsComponent,
+    SectionComponent,
+    EditorComponent,
+    ViewComponent,
+    
+    
+      ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    CommonModule,
+    BsDropdownModule.forRoot(),
+    TooltipModule.forRoot(),
+    ModalModule.forRoot()
   ],
+  exports: [BsDropdownModule, TooltipModule, ModalModule],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+export class AppBootstrapModule {}
